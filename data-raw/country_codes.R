e8b768ae786ff7e09dc801fe3352e23de2c05775
charliejhadley <charliejhadley@gmail.com>
2020-06-30 15:39:46 +0100

    export country_codes

--- data-raw/country_codes.R
+++ data-raw/country_codes.R
@@ -5,3 +5,6 @@ country_codes <- codelist %>%
   select(country.name.en, iso2c) %>%
   filter(complete.cases(.)) %>%
   set_names(c("country_name", "country_code"))
+
+save(country_codes, file = "data/country_codes.RData")
+
