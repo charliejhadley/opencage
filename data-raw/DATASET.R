7d422ad260ce46644975bd9f937948a24e3d4beb
charliejhadley <charliejhadley@gmail.com>
2020-06-30 15:41:23 +0100

    export country_codes

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -5,3 +5,5 @@ country_codes <- codelist %>%
   select(country.name.en, iso2c) %>%
   filter(complete.cases(.)) %>%
   set_names(c("country_name", "country_code"))
+
+usethis::use_data(country_codes, overwrite = TRUE)
